4f648b4c63924fecbafed098503174ba30e6bda6
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-19 23:12:19 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     self.artists.name
+    #artists.pluck(:name)
     # return an array of strings containing every musician's name
   end
 end
