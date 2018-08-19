90d8d59c7bba90a84408db78a857d0793eeec132
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-19 22:53:08 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,7 +13,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    artists_id.name.all
+    artist_id.name.all
     # return an array of strings containing every musician's name
   end
 end
