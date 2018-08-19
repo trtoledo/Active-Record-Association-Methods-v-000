8582b7abf71d3c36d492fd54697d7b7ba0b838e4
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-19 22:44:02 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,7 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    song.first.genre
+    self.songs.first.genre
     #return the genre of the artist's first saved song
   end
 
